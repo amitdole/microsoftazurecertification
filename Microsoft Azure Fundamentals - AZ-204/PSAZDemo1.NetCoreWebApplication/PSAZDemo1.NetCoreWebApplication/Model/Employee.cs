c398283082218177fd58fa4754c30939badfa4fe
amitdole <36283193+amitdole@users.noreply.github.com>
2022-09-11 09:41:42 -0700

    204

---
+++ Microsoft Azure Fundamentals - AZ-204/PSAZDemo1.NetCoreWebApplication/PSAZDemo1.NetCoreWebApplication/Model/Employee.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSAZDemo1.NetCoreWebApplication.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [BindProperty]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [BindProperty]
+
+        public DateTime JoiningDate { get; set; }
+    }
+}
---
+++ Microsoft Azure Fundamentals - AZ-204/PSAZDemo1.NetCoreWebApplication/PSAZDemo1.NetCoreWebApplication/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using PSAZDemo1.NetCoreWebApplication.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("AzureDatabase");
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(connectionString), ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    try
+    {
+        dbContext.Database.EnsureCreated();
+    }
+    catch(Exception e)
+    {
+
+    }
+    
+}
+
+app.Run();
